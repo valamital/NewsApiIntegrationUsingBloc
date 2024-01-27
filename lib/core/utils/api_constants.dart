428547e5c19f943a651fca73ce68eb25ec1ec6ca
ganesha <valamital11@gmail.com>
2024-01-28 06:02:33 +1000

    Required changes

--- lib/core/utils/api_constants.dart
+++ lib/core/utils/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstant {
   String newsApiUrl =
-      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=74308df19e704769b88c4e5b6f2f4a04';
+      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey= your_actual_api_key';
 }
